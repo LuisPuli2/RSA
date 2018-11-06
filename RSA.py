1248900a040f3b126c1089e041bc8f106dd17127
luisPuli2 <lugia365@gmail.com>
2018-11-06 16:01:19 -0600

    vos sabés en cambio extraer de ese páramo, mi germen de alegría y regarlo mirándolo

---
+++ RSA.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# RSA
+
+# Recibe dos números primos
+def generaLlaves ():
+	# Generamos dos números primos aleatorios.
+	# Calcular N = p*q
+	# Phi(N) = (p-1)(q-1)
+	# Encontrar e que sea coprimo con N
+	# Calcular d que sea el inverso multiplicativo de e mod N
+	pass
+
+def cifra ():
+	pass
+
+def decifra ():
+	pass 
